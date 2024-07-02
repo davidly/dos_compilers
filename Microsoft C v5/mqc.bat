696d36e2828c908553c8b8d056b0b49f9a77e6a9
davidly <davidly_lee@hotmail.com>
2024-07-02 06:12:56 -0700

    build fix for spaces in path names

--- Microsoft C v5/mqc.bat
+++ Microsoft C v5/mqc.bat
@@ -3,10 +3,10 @@
 del %1.exe
 del %1.obj
 
-ntvdm -h qcl /DDOSTIME /AS /Ot /Gs /c -I inc -I inc\sys %1.c
+ntvdm -r:. -h qcl /DDOSTIME /AS /Ot /Gs /c -I inc -I inc\sys %1.c
 if %ERRORLEVEL% NEQ 0 goto alldone
 
-ntvdm -h -t link %1,,%1,lib\slibce,lib\quicklib.obj
+ntvdm -r:. -h -t link %1,,%1,lib\slibce,lib\quicklib.obj
 if %ERRORLEVEL% NEQ 0 goto alldone
 
 ntvdm -p %1
