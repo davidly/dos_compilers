954ae9ea618245ca14a3f31f31a2122a78630f34
David Lee <davidly_lee@hotmail.com>
2024-07-03 16:06:36 -0700

    linux build

--- Borland Turbo C v2/m.bat
+++ Borland Turbo C v2/m.bat
@@ -1,5 +1,5 @@
 del %1.com 1>nul 2>nul
-rem ntvdm -r:. -c -t tcc -lt -DDOSTIME -G -O -Z -mt -Iinclude -Llib -r -e%1.com %1.c
-ntvdm -r:. -c -t tcc -ls -DDOSTIME -G -O -Z -ms -Iinclude -Llib -r -e%1.exe %1.c
+rem ntvdm -r:. -c tcc -lt -DDOSTIME -G -O -Z -mt -Iinclude -Llib -r -e%1.com %1.c
+ntvdm -r:. -c tcc -ls -DDOSTIME -G -O -Z -ms -Iinclude -Llib -r -e%1.exe %1.c
 ntvdm -r:. -c -p %1
 
