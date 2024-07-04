5733540a3b480d4c3b82323d4d3277f1b83f6efb
David Lee <davidly_lee@hotmail.com>
2024-07-04 06:32:35 -0700

    linux build

---
+++ DeSmet C88 v24/m.sh
@@ -0,0 +1,12 @@
+str=${1^^}
+
+rm $str.O 2>/dev/null
+rm $str.EXE 2>/dev/null
+
+# -nDESMETC doesn't work in this version of c88
+ntvdm -r:. -u c88 $str
+echo
+ntvdm -r:. -u bind $str other
+echo
+
+rm $str.O 2>/dev/null
