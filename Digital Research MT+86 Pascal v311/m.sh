ebc6c9f2b2d1089f5d36d6cdc5bb0729b5665b98
David Lee <davidly_lee@hotmail.com>
2024-07-04 06:42:03 -0700

    linux build

---
+++ Digital Research MT+86 Pascal v311/m.sh
@@ -0,0 +1,14 @@
+str=${1^^}
+
+rm $str.ERR 2>/dev/null
+rm $str.R86 2>/dev/null
+rm $str.EXE 2>/dev/null
+
+ntvdm -r:. -u mt86 $str
+ntvdm -r:. -u linkmt $str paslib.r86 fpreals.r86
+
+rm $str.ERR 2>/dev/null
+rm $str.R86 2>/dev/null
+
+ntvdm -c -p $str
+
