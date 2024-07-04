ab6bb1fe97135558460b21ec00f1cd8ac74c164f
David Lee <davidly_lee@hotmail.com>
2024-07-04 09:22:45 -0700

    don't trace

--- Mark Williams MWC v311/code/m.sh
+++ Mark Williams MWC v311/code/m.sh
@@ -3,6 +3,6 @@ str=$(tr '[a-z]' '[A-Z]' <<< $1)
 rm $str.O 2>/dev/null
 rm $str.EXE 2>/dev/null
 
-ntvdm -t -u -r:.. "-e:CCHEAD=@C:\lib\cc -f -DMWC" ..\\bin\\cc $str.c -lm
+ntvdm -u -r:.. "-e:CCHEAD=@C:\lib\cc -f -DMWC" ..\\bin\\cc $str.c -lm
 
 rm $str.O 2>/dev/null
