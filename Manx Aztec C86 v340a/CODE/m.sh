f9d236fac46156f1fec07a7751360ce36e712fe5
David Lee <davidly_lee@hotmail.com>
2024-07-04 08:47:03 -0700

    linux build

---
+++ Manx Aztec C86 v340a/CODE/m.sh
@@ -0,0 +1,10 @@
+str=$(tr '[a-z]' '[A-Z]' <<< $1)
+
+rm $str.O 2>/dev/null
+rm $str.EXE 2>/dev/null
+
+ntvdm -u -r:.. -e:include=c:\\include,path=c:\\bin ..\\bin\\cc -DAZTEC86 +F +A $str.c
+rm ctmp* 2>dev/null
+ntvdm -u -r:.. -e:lib=c:\\lib ..\\bin\\ln $str c:\\lib\\m.lib c:\\lib\\c.lib
+
+rm $str.O 2>/dev/null
