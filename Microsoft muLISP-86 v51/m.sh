6ce452432efa0ea928db660e3753c2a2019a984f
davidly <davidly_lee@hotmail.com>
2024-07-05 09:15:22 -0700

    macos changes

--- Microsoft muLISP-86 v51/m.sh
+++ Microsoft muLISP-86 v51/m.sh
@@ -6,9 +6,10 @@ cat COMMON.LSP STRUCTUR.LSP $str.LSP >FOO.LSP
 tr '\32' ' ' <FOO.LSP >BAR.LSP
 
 # remove (RDS) twice from the file
-sed -e '/(RDS)/d' -i BAR.LSP
+sed -e '/(RDS)/d' BAR.LSP >BAZ.LSP
 
-ntvdm -r:. -u -p -c mulisp BAR.LSP
+ntvdm -r:. -u -p -c mulisp BAZ.LSP
 
 rm FOO.LSP 2>/dev/null
 rm BAR.LSP 2>/dev/null
+rm BAZ.LSP 2>/dev/null
