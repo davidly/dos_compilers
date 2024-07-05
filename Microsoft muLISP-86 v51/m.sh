7d79fb20f66cb8ed4a0ecd43e404d1f78b581a0f
David Lee <davidly_lee@hotmail.com>
2024-07-05 08:59:05 -0700

    linux build

---
+++ Microsoft muLISP-86 v51/m.sh
@@ -0,0 +1,14 @@
+str=$(tr '[a-z]' '[A-Z]' <<< $1)
+
+cat COMMON.LSP STRUCTUR.LSP $str.LSP >FOO.LSP
+
+# remove ^z end of file characters found in common.lsp and structur.lsp
+tr '\32' ' ' <FOO.LSP >BAR.LSP
+
+# remove (RDS) twice from the file
+sed -e '/(RDS)/d' -i BAR.LSP
+
+ntvdm -r:. -u -p -c mulisp BAR.LSP
+
+rm FOO.LSP 2>/dev/null
+rm BAR.LSP 2>/dev/null
