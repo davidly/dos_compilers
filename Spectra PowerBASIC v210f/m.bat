8af2e2e1af9e655fc9163f21f099bab5f40d2e74
davidly <davidly_lee@hotmail.com>
2024-07-06 12:01:00 -0700

    Spectra PowerBASIC v2.10f

---
+++ Spectra PowerBASIC v210f/m.bat
@@ -0,0 +1,3 @@
+rem PowerBASIC has no command-line build. You have to start the app, load the file, set the main file, and compile.
+rem Be sure to check the compilation defaults to turn off debug settings go get substantial performance wins
+
---
+++ Spectra PowerBASIC v210f/runit.bat
@@ -0,0 +1,3 @@
+rem runs a PowerBASIC app, which only write to CGA memory for text output
+ntvdm -c -p -m %1.EXE
+
