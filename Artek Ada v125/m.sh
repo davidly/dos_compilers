52c763c19bbb64c7ce761f15e5a4033779f28a0f
David Lee <davidly_lee@hotmail.com>
2024-07-08 09:36:52 -0700

    linux build

---
+++ Artek Ada v125/m.sh
@@ -0,0 +1,17 @@
+str=$(tr '[a-z]' '[A-Z]' <<< $1)
+
+rm $str.AXE 2>/dev/null
+rm $str.EXE 2>/dev/null
+
+# /n removes array bounds checks and must be after the source file
+ntvdm -r:. -u ada $str.ada /n
+ntvdm -r:. -u linklib $str
+
+# interpreter runs on the .AXE file
+ntvdm -r:. -u -c -p ai $str
+
+# native code. I've found a86 to be buggy.
+#    note: a86 produces bad code for ttt.ada. it uses inconsistent addresses for the "board" array.
+#    note2: a86 produces bad code for e.ada but the workaround to use more local variables worked.
+ntvdm -r:. -u a86 $str.axe /n
+ntvdm -r:. -u -c -p $str.exe
