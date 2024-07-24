564f733fc405379a7783253052979c8364a53820
David Lee <davidly_lee@hotmail.com>
2024-07-24 14:19:27 -0700

    linux build instructions

---
+++ Zedcor ZBasic v402/m.sh
@@ -0,0 +1,8 @@
+echo "I can't figure out a way to compile from the command line."
+echo "Enter this to build in ZBasic:"
+echo "   ntvdm -r:. -u ZBASIC SAMPLE.BAS"
+echo "   run*"
+echo "   SAMPLE.COM"
+echo "   system"
+echo "   ntvdm -c -p SAMPLE.COM"
+
