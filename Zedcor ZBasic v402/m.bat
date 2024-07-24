c79505a93c18897513eb924c329a8bd6f4001aa6
davidly <davidly_lee@hotmail.com>
2024-07-24 14:14:57 -0700

    Zedcor ZBasic v4.02

---
+++ Zedcor ZBasic v402/m.bat
@@ -0,0 +1,8 @@
+echo I can't figure out a way to compile from the command line.
+echo Enter this to build in ZBasic:
+echo    ntvdm zbasic sample.bas
+echo    run*
+echo    sample.com
+echo    system
+echo    ntvdm -c -p sample.com
+
