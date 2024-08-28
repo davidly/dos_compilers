cfcd89ff8742579a8ac035f0618192d334a55a48
David Lee <davidly_lee@hotmail.com>
2024-08-28 07:21:42 -0700

    tc++ makes .exe, not .com files

--- Borland Turbo C++ v1/BIN/m.sh
+++ Borland Turbo C++ v1/BIN/m.sh
@@ -1,7 +1,7 @@
 str=$(tr '[a-z]' '[A-Z]' <<< $1)
 
 rm $str.OBJ 2>/dev/null
-rm $str.COM 2>/dev/null
+rm $str.EXE 2>/dev/null
 
 ntvdm -u -r:.. tcc  -DDOSTIME -ms -Z -O -G -I..\\include -L..\\lib -e$str.EXE $str.C
 
